88bca928b25eb58ca48c40709128b861a23511d4
libobz <hebobibun@gmail.com>
2023-01-11 21:14:52 +0700

    update config

--- config/config.go
+++ config/config.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/spf13/viper"
 )
@@ -24,19 +26,49 @@ func InitConfig() *AppConfig {
 func ReadEnv() *AppConfig {
 	app := AppConfig{}
 
-	viper.AddConfigPath(".")
-	viper.SetConfigName("local")
-	viper.SetConfigType("env")
+	isRead := true
 
-	err := viper.ReadInConfig()
-	if err != nil {
-		log.Println("error read config : ", err.Error())
-		return nil
+	if val, found := os.LookupEnv("JWT_KEY"); found {
+		app.jwtKey = val
+		isRead = false
 	}
-	err = viper.Unmarshal(&app)
-	if err != nil {
-		log.Println("error parse config : ", err.Error())
-		return nil
+	if val, found := os.LookupEnv("DBUSER"); found {
+		app.DBUser = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DBPASS"); found {
+		app.DBPass = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DBHOST"); found {
+		app.DBHost = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DBPORT"); found {
+		cnv, _ := strconv.Atoi(val)
+		app.DBPort = cnv
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DBNAME"); found {
+		app.DBName = val
+		isRead = false
+	}
+
+	if isRead {
+		viper.AddConfigPath(".")
+		viper.SetConfigName("local")
+		viper.SetConfigType("env")
+
+		err := viper.ReadInConfig()
+		if err != nil {
+			log.Println("error read config : ", err.Error())
+			return nil
+		}
+		err = viper.Unmarshal(&app)
+		if err != nil {
+			log.Println("error parse config : ", err.Error())
+			return nil
+		}
 	}
 
 	JWTKey = app.jwtKey
